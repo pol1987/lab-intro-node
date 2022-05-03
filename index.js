3536b61f34cffe6e43bf14554a97b1ecb3c245e0
Pablo Orozco <78923863+pol1987@users.noreply.github.com>
2022-05-03 18:48:14 +0200

    lab completo salvo la ultima iteracion avg

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ class SortedList {
   add(item) {
     this.items.push(item);
     this.items.sort((a, b) => a - b);
+    this.length++
   }
   get(pos) {
     if (this.items[pos]) {
       return this.items[pos];
     } else {
       throw new Error("OutOfBounds");
-    }
+    }    
   }
 
   max() {
